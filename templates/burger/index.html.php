055670e953ed103b1aee209fbda01ef00ca8eae7
enzo <enzovcnt@gmail.com>
2025-02-14 15:33:32 +0100

    template index work and database work good

--- templates/burger/index.html.php
+++ templates/burger/index.html.php
@@ -1,1 +1,10 @@
-<h1>Je suis burger</h1>
+<h1>The burgers</h1>
+
+<?php foreach ($burgers as $burger): ?>
+
+<h2><?= $burger->getTitle()?></h2>
+<p><?= $burger->getContent()?></p>
+
+<a href="/burger/show?id=<?=$burger->getId() ?>" class="btn btn-primary">Read</a>
+
+<?php endforeach; ?>
