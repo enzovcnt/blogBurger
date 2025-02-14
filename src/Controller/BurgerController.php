2402e2d0db77c2e162cb48296c48e817a7c9d7b7
enzo <enzovcnt@gmail.com>
2025-02-14 15:44:23 +0100

    create route show and template show + create link return for show

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -6,15 +6,35 @@ use App\Entity\Burger;
 use Attributes\DefaultEntity;
 use Core\Attributes\Route;
 use Core\Controller\Controller;
+use Core\Http\Response;
 
 #[DefaultEntity(entityName: Burger::class)]
 class BurgerController extends Controller
 {
-    #[Route('/burgers', routeName: 'burgers')]
+    #[Route(uri:'/burgers', routeName: 'burgers')]
     public function index()
     {
         return $this->render('burger/index', [
             "burgers" => $this->getRepository()->findAll(),
         ]);
+    }
+
+    #[Route(uri:'/burger/show', routeName: 'burger')]
+    public function show():Response
+    {
+        $id = $this->getRequest()->get(["id"=>"number"]);
+
+        if(!$id)
+        {
+            return $this->redirectToRoute('burgers');
+        }
+
+        $burger = $this->getRepository()->find($id);
+
+        if(!$burger)
+        {
+            return $this->redirectToRoute('burgers');
+        }
+        return $this->render('burger/show', ["burger" => $burger]);
     }
 }
---
+++ templates/burger/show.html.php
@@ -0,0 +1,8 @@
+<h2>The burger</h2>
+<div class="border border-blue">
+    <h2><?= $burger->getTitle()?></h2>
+    <p><?= $burger->getContent() ?></p>
+
+</div>
+
+<a href="/burgers" class="btn btn-secondary">Return</a>
